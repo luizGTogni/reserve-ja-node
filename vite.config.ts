5a049c50549cf76647c4f1bbaf8a82e34bdae6ea
Luiz Togni <contato.togni@gmail.com>
2025-09-14 17:14:09 -0300

    chore: add vite config tests

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,28 @@ export default defineConfig({
   test: {
     dir: "src",
     globals: true,
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          dir: "src/application/use-cases",
+          include: ["**/*.{test,unit.spec}.?(c|m)[jt]s?(x)"],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "integration",
+          dir: "src/application/use-cases",
+          include: ["**/*.{test,integration.spec}.?(c|m)[jt]s?(x)"],
+          environment:
+            "./prisma/vitest-environment-prisma/prisma-test-environment.ts",
+        },
+      },
+    ],
     coverage: {
-      include: ["src/use-cases/**"],
+      include: ["src/application/use-cases/**"],
     },
   },
 });
