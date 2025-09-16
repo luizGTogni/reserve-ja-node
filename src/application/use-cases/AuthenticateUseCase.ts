431de6e01a49a90a897fbb6b12a46b0cb892d92a
Luiz Togni <contato.togni@gmail.com>
2025-09-15 21:20:58 -0300

    feat(application): add authenticate use case

---
+++ src/application/use-cases/AuthenticateUseCase.ts
@@ -0,0 +1,42 @@
+import type { User } from "@/domain/entities/User";
+import { InvalidCredentialsError } from "../errors/InvalidCredentialsError";
+import type { IUsersRepository } from "../repositories/IUsersRepository";
+import type { IHasher } from "../services/IHasher";
+
+type AuthenticateRequest = {
+  email: string;
+  password: string;
+};
+
+type AuthenticateResponse = {
+  user: User;
+};
+
+export class AuthenticateUseCase {
+  constructor(
+    private readonly usersRepository: IUsersRepository,
+    private readonly hasher: IHasher,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateRequest): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await this.hasher.compare({
+      plain: password,
+      hashed: user.password_hash,
+    });
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return { user };
+  }
+}
