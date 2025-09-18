2f74b1a449e4c9d82733d32b54a034f15899b28b
Luiz Togni <contato.togni@gmail.com>
2025-09-17 21:22:00 -0300

    feat(application): add create property use case

---
+++ src/application/use-cases/properties/CreatePropertyUseCase.ts
@@ -0,0 +1,40 @@
+import { ValueInvalidError } from "@/application/errors/ValueInvalidError";
+import type { IPropertiesRepository } from "@/application/repositories/IPropertiesRepository";
+import type { Property } from "@/domain/entities/Property";
+
+interface CreatePropertyRequest {
+  userId: string;
+  title: string;
+  description: string;
+  latitude: number;
+  longitude: number;
+  price_base: number;
+  capacity: number;
+  refund_policy: "TOTAL" | "PARCIAL";
+}
+
+interface CreatePropertyResponse {
+  property: Property;
+}
+
+export class CreatePropertyUseCase {
+  constructor(private readonly propertiesRepository: IPropertiesRepository) {}
+
+  async execute(data: CreatePropertyRequest): Promise<CreatePropertyResponse> {
+    if (data.price_base <= 0) {
+      throw new ValueInvalidError("Price Base");
+    }
+
+    if (data.capacity <= 0) {
+      throw new ValueInvalidError("Capacity");
+    }
+
+    const property = await this.propertiesRepository.create({
+      ...data,
+      host_id: data.userId,
+      amenities: [],
+    });
+
+    return { property };
+  }
+}
